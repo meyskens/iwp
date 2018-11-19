6e75ed616d36b19c3a816e6fbb8be579615e6d57
Maartje Eyskens <meyskens@me.com>
2018-11-19 11:52:49 +0100

    move out js code to transpile

---
+++ frontend/app/vm.js
@@ -0,0 +1,29 @@
+var am = new Vue({
+    el: '#app',
+    data: {
+        individual: true,
+        notary: true,
+        agency: false,
+
+        loading: false,
+
+        postcode: "",
+        saleType: "",
+
+        results: []
+    },
+    methods: {
+        lookup() {
+            this.loading = true
+            axios.get(`/api/lookup?postcode=${this.postcode}&saleType=${this.saleType}&individual=${this.individual ? "true" : ""}&notary=${this.notary ? "true" : ""}&agency=${this.agency ? "true" : ""}`)
+            .then((res) => {
+                this.results = res.data
+                this.loading = false
+            })
+        },
+        openBrowser(url) {
+            console.log(url)
+            goapp.openURL(url)
+        }
+    }
+})
